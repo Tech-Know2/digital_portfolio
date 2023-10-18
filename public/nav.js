885b27629c61f9878c257a7c171c8f7fd235a58e
LetoTwo <Cooperharris727@gmail.com>
2023-10-17 19:53:58 -0700

    next steps

---
+++ public/nav.js
@@ -0,0 +1,14 @@
+function createNavbar() {
+    const navbar = document.createElement('ul');
+    navbar.innerHTML = `
+        <li><a class="active" href="/index.html">Home</a></li>
+        <li><a href="/aboutme.html">About Me</a></li>
+        <li><a href="#contact">Projects</a></li>
+        <li><a href="#about">Languages</a></li>
+        <li><a href="#about">Certifications</a></li>
+    `;
+    
+    document.body.prepend(navbar);
+}
+
+window.addEventListener('DOMContentLoaded', createNavbar);
